0087bce52d0156145423930486005a0637ba4ad1
mngaonkar <mahadev.gaonkar@gmail.com>
2014-05-26 00:22:13 +0530

    setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='pydownloader',
+      version='0.1',
+      description='HTTP downloader',
+      author='Mahadev N Gaonkar',
+      license='BSD',
+      author_email='mahadev.gaonkar@gmail.com',
+      url='https://github.com/mngaonkar/pydownloader',
+      packages=['.'],
+     )
